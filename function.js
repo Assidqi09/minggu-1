bad54995bbcb567d27cdb9c83b99f0a90f70529b
Assidqi09 <syechanniandi.a@gmail.com>
2022-01-18 14:38:47 +0700

    function

---
+++ function.js
@@ -0,0 +1,27 @@
+// No.1
+function shoutOut(){
+return "hallo function"
+}
+var tampung = shoutOut();
+console.log (tampung);
+
+//nomor 2
+function calculateMultiply(numb1,numb2){
+    return 5*6
+}
+var numb1 = 5;
+var numb2 = 6;
+var hasilperkalian= calculateMultiply(numb1, numb2);
+console.log(hasilperkalian);
+
+// nomor 3
+function processSentence(){
+    return "Nama saya"+' '+this.name +' '+"umur saya"+' '+ this.age+' '+ "tahun, alamat saya di"+' '+this.address+' ' + "dan saya punya hobby yaitu"+ ' '+ this.hobby
+    }        
+nama = "Agus";
+age = "30";
+address = "Jln. Malioboro, Yogjakarta";
+hobby = "gaming";
+
+var fullSentence = processSentence(nama,age,address,hobby);
+console.log(fullSentence);
